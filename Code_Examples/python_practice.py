d040ff9cd8dba2774719b9d5ae793f8107055ac0
deshayk <deshaykidd@gmail.com>
2022-04-05 05:34:45 -0500

    update class

--- Code_Examples/python_practice.py
+++ Code_Examples/python_practice.py
@@ -1,4 +1,4 @@
-def Work(): # class definition
+def Job(): # class definition
   
     def employee(self, name, email, pay): # method definition
         self.name = name # instance variable
@@ -8,12 +8,12 @@ def Work(): # class definition
     def employer(self, name): # method definition
         self.name = name # instance variable
         
-w1 = Work() # create instance
+w1 = Job() # create instance
 w1.employee('John', 'johnjacob@python.inc', '$100') # call method
 print(w1.name) # access instance variable
 print(w1.email) # access instance variable
 print(w1.pay) # access instance variable
 
-j1 = Work() # create instance
+j1 = Job() # create instance
 j1.employer('Python Inc.') # call method
 print(j1.name) # access instance variable
