aa253877c87b0884c6ba695932b43e0f629beb7b
deshayk <deshaykidd@gmail.com>
2022-04-11 03:26:31 -0500

    update car class comments

--- car.py
+++ car.py
@@ -13,3 +13,12 @@ class Car: # establishes Car class
   def brake(self): # method (action) that indicates the car is stopping
       print("The brakes have been hit. The car is stopping.")
     
+car_1 = Car("Honda", "Civic", "2019", "Red") #instantiate a new object of the class Car with the required arguments
+car_2 = Car("Toyota", "Corolla", "2020", "Blue") #instantiate a new object of the class Car with the required arguments
+car_3 = Car("Ford", "F150", "2021", "White") #instantiate a new object of the class Car with the required arguments
+car_4 = Car("Chevy", "Camaro", "2022", "Black") #instantiate a new object of the class Car with the required arguments
+car_5 = Car("Dodge", "Charger", "2023", "Yellow") #instantiate a new object of the class Car with the required arguments
+
+car_1.drive()
+car_3.drive()
+car_5.drive()
