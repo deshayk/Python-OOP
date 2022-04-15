2b2260c151fb13b3a6678855669df0e4cd9775d3
deshayk <deshaykidd@gmail.com>
2022-04-15 16:48:59 -0500

    move file

---
+++ Code_Examples/main.py
@@ -0,0 +1,3 @@
+from Code_Examples.car import Car # imports the Car class from the car.py module
+from Code_Examples.student import Student # imports the Student class from the student.py module
+
--- main.py
+++
@@ -1,3 +0,0 @@
-from car import Car # imports the Car class from the car.py module
-from student import Student # imports the Student class from the student.py module
-
